8261d3b0deb40f1dbd4921a62e4e2cc0aa2593d4
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-05-21 02:51:31 +0700

    fix: fix layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,5 @@
-// app/dashboard/layout.tsx
-import { Footer7 } from "@/components/footer7";
 import MainLayout from "@/components/mainLayout";
-import { Navbar5 } from "@/components/navbar5";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { cn } from "@/lib/utils";
 
 export default function DashboardLayout({
   children,
