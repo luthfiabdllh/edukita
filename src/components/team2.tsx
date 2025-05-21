ab1ef11b634a7cffafda0dff1abdd9e1f53cbec0
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-05-21 09:43:35 +0700

    fix: fix words team components

--- src/components/team2.tsx
+++ src/components/team2.tsx
@@ -1,61 +1,79 @@
-import { Dribbble, Github, Linkedin } from "lucide-react";
+import { Github, Instagram, Linkedin } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 const people = [
   {
     id: "person-1",
-    name: "Name",
-    role: "Role",
+    name: "Dimal Karim Ahmad",
+    role: "Supervisor",
     description:
-      "Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
+      "Oversees project execution, ensuring that all aspects run smoothly and meet quality standards.",
     avatar: "https://shadcnblocks.com/images/block/avatar-1.webp",
+    github: "#",
+    linkedin: "https://www.linkedin.com/in/dimal-karim-ahmad/",
+    instagram: "#",
   },
   {
     id: "person-2",
-    name: "Name",
-    role: "Role",
-    description: "Elig doloremque mollitia fugiat omnis!",
+    name: "Rifqi Renaldo",
+    role: "UI/UX Designer",
+    description: "Designs user-friendly interfaces to provide an optimal and seamless user experience.",
     avatar: "https://shadcnblocks.com/images/block/avatar-2.webp",
+    github: "#",
+    linkedin: "https://www.linkedin.com/in/rifqi-renaldo-2439892b8/",
+    instagram: "#",
   },
   {
     id: "person-3",
-    name: "Name",
-    role: "Role",
-    description:
-      "Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
-    avatar: "https://shadcnblocks.com/images/block/avatar-3.webp",
+    name: "Ahmad Luthfi Abdillah",
+    role: "Frontend Developer",
+    description: "Develops responsive visual elements and interactions to enhance application performance.",
+    avatar: "https://shadcnblocks.com/images/block/avatar-4.webp",
+    github: "https://github.com/luthfiabdllh",
+    linkedin: "http://linkedin.com/in/luthfi-abdllh/",
+    instagram: "https://instagram.com/luthfi_abdllh",
   },
   {
     id: "person-4",
-    name: "Name",
-    role: "Role",
-    description: "Elig doloremque mollitia fugiat omnis!",
-    avatar: "https://shadcnblocks.com/images/block/avatar-4.webp",
+    name: "Devangga Arya Hartanta",
+    role: "Frontend Developer",
+    description: "Builds functional UI components while ensuring an appealing and efficient design",
+    avatar: "https://shadcnblocks.com/images/block/avatar-3.webp",
+    github: "#",
+    linkedin: "https://www.linkedin.com/in/devangga-arya-747550229",
+    instagram: "#",
   },
   {
     id: "person-5",
-    name: "Name",
-    role: "Role",
-    description:
-      "Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
+    name: "Asyifa Dzaky Maulana A",
+    role: "Backend Developer",
+    description: "Manages system architecture and database operations to support backend stability.",
     avatar: "https://shadcnblocks.com/images/block/avatar-5.webp",
+    github: "#",
+    linkedin: "https://www.linkedin.com/in/asyifa-dzaky-maulana-aditria/",
+    instagram: "#",
   },
   {
     id: "person-6",
-    name: "Name",
-    role: "Role",
-    description:
-      "Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
-    avatar: "https://shadcnblocks.com/images/block/avatar-6.webp",
+    name: "Aurelius Bevan Yudira P",
+    role: "Backend Developer",
+    description: "Develops business logic and APIs to ensure a scalable and reliable system.",
+    avatar: "https://shadcnblocks.com/images/block/avatar-7.webp",
+    github: "#",
+    linkedin: "#",
+    instagram: "#",
   },
   {
     id: "person-7",
-    name: "Name",
-    role: "Role",
-    description:
-      "Elig doloremque mollitia fugiat omnis! Porro facilis quo animi consequatur. Explicabo.",
-    avatar: "https://shadcnblocks.com/images/block/avatar-7.webp",
+    name: "Muhammad Najwan Fadlillah",
+    role: "Data Scientist",
+    description: "Analyzes and processes data to generate insights that aid decision-making within the project.",
+    avatar: "https://shadcnblocks.com/images/block/avatar-6.webp",
+    github: "#",
+    linkedin: "https://www.linkedin.com/in/muhammadnajwanf/",
+    instagram: "#",
   },
 ];
 
@@ -63,12 +81,12 @@ const Team2 = () => {
   return (
     <section className="py-32">
       <div className="container flex flex-col items-start text-left">
-        <p className="semibold">Our team</p>
+        <p className="semibold">Meet Our team</p>
         <h2 className="my-6 text-2xl font-bold text-pretty lg:text-4xl">
-          The team you&apos;ll be working with
+          The contributors in this project bring expertise and dedication to success.
         </h2>
         <p className="mb-8 max-w-3xl text-muted-foreground lg:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          We are committed to collaboration, ensuring innovation, efficiency, and high-quality outcomes.
         </p>
       </div>
       <div className="container mt-16 grid gap-x-12 gap-y-16 md:grid-cols-2 lg:grid-cols-4">
@@ -84,15 +102,15 @@ const Team2 = () => {
               {person.description}
             </p>
             <div className="mt-2 flex gap-4">
-              <a href="#">
+              <Link href={person.github}>
                 <Github className="size-5 text-muted-foreground" />
-              </a>
-              <a href="#">
+              </Link>
+              <Link href={person.linkedin}>
                 <Linkedin className="size-5 text-muted-foreground" />
-              </a>
-              <a href="#">
-                <Dribbble className="size-5 text-muted-foreground" />
-              </a>
+              </Link>
+              <Link href={person.instagram}>
+                <Instagram className="size-5 text-muted-foreground" />
+              </Link>
             </div>
           </div>
         ))}
